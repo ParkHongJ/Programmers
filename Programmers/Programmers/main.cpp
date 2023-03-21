9ec6494bb0e8c946c680aff06b9ce99b53ff42f1
DESKTOP-E55IUE1\JJUNI <qkrghdwns98@naver.com>
2023-03-21 20:10:17 +0900

    프로그래머스_푸드파이터 대회

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,80 +1,47 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <set>
+
 using namespace std;
 
-int solution(vector<vector<int>> board) {
-    int answer = 0;
-
-    //안전지대의 인덱스
-    set<int> BoomIndex;
-
-    int iMaxSize = board.size() * board.size();
-    for (int i = 0; i < board.size(); i++)
+string solution(vector<int> food) {
+    string answer = "";
+    string Half = "";
+    int iSize = 0;
+    for (int i = 1; i < food.size(); i++)
     {
-        for (int j = 0; j < board[i].size(); j++)
-        {
-            //지뢰라면.
-            if (board[i][j] == 1)
-			{
-				BoomIndex.insert(i * board[i].size() + j);
+        int iHalf = food[i] % 2;
 
-                //왼쪽이 있다면.
-                if (j - 1 >= 0)
-                {
-                    BoomIndex.insert(i * board[i].size() + (j - 1));
-                    
-                    //좌상
-                    if (i - 1 >= 0)
-                    {
-                        BoomIndex.insert((i - 1) * board[i].size() + (j - 1));
-                    }
-                    //좌하
-                    if (i + 1 < board.size())
-                    {
-                        BoomIndex.insert((i + 1) * board[i].size() + (j - 1));
-                    }
-                }
-                //오른쪽
-                if (j + 1 < board[i].size())
-                {
-                    BoomIndex.insert(i * board[i].size() + (j + 1));
-                    //우상
-                    if (i - 1 >= 0)
-                    {
-                        BoomIndex.insert((i - 1) * board[i].size() + (j + 1));
-                    }
-                    //우하
-                    if (i + 1 < board.size())
-                    {
-                        BoomIndex.insert((i + 1) * board[i].size() + (j + 1));
-                    }
-                }
-                //상
-                if (i - 1 >= 0)
-                {
-                    BoomIndex.insert((i - 1) * board[i].size() + j);
-                }
-                //하
-                if (i + 1 < board.size() )
-                {
-                    BoomIndex.insert((i + 1) * board[i].size() + j);
-                }
+		//음식이 한개라면 버림
+		if (food[i] == 1)
+		{
+			continue;
+		}
+		else
+		{
+			//음식은 배치할 수 있지만 홀수인 경우
+            if (iHalf != 0)
+            {
+                food[i] -= 1;
             }
+		}
+
+        for (int j = 0; j < food[i] / 2; j++)
+        {
+            Half += to_string(i);
         }
     }
-    return iMaxSize - BoomIndex.size();
+    answer += Half + "0";
+    for (int i = Half.length() - 1; i >= 0; i--)
+    {
+        answer += Half[i];
+    }
+    return answer;
 }
 
 int main()
 {
-    vector<vector<int>> common = {
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0},
-        {0, 0, 1, 0, 0},
-        {0, 0, 0, 0, 0}};
-    cout << solution(common) << endl;
+    vector<int> food = { 1, 7, 1, 2 };
+    cout << solution(food) << endl;
     return 0;
 }
