a879abe4483cef75bb34db50545d793ff9bf5bab
DESKTOP-E55IUE1\JJUNI <qkrghdwns98@naver.com>
2023-03-29 13:22:23 +0900

    백준 그리디 알고리즘 동전0문제 + 백준 입력값 셋팅

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,60 +1,41 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <stack>
-#include <algorithm>
+#include <conio.h>
+#pragma warning(disable :4996)
 using namespace std;
 
-int solution(int n, vector<int> lost, vector<int> reserve) {
-    int answer = 0;
-
-    sort(lost.begin(), lost.end());
-    sort(reserve.begin(), reserve.end());
-
-    for (int i = 0; i < lost.size(); i++)
-    {
-        for (int j = 0; j < reserve.size(); j++)
-        {
-            if (lost[i] == reserve[j])
-            {
-                lost.erase(lost.begin() + i);
-                reserve.erase(reserve.begin() + j);
-            }
-        }
-    }
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 0; j < lost.size(); j++)
-        {
-            //도난당했다면
-            if (lost[j] == i)
-            {
-                answer--;
-                for (int k = 0; k < reserve.size(); k++)
-                {
-                    if ((reserve[k] -1 == lost[j]) || (reserve[k] + 1 == lost[j]) || (reserve[k] == lost[j]))
-                    {
-                        answer++;
-                        lost.erase(lost.begin() + j);
-                        reserve.erase(reserve.begin() + k);
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        answer++;
-    }
-
-    return answer;
-}
-
+int N, M;
 int main()
 {
-	vector<int> lost = { 2,3 };
-	vector<int> reserve = { 3,4 };
-    //13[13, 6, 1][11, 9, 8, 7] 11
-    cout << solution(4, lost, reserve) << endl;
+    freopen("input.txt", "r", stdin);
+
+    vector<int> arr;
+    cin >> N;
+    cin >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        int a;
+        cin >> a; 
+        arr.push_back(a);
+    }
+
+    int iCount = 0;
+    for (int i = arr.size() - 1; i >= 0; i--)
+    {
+        if (arr[i] > M)
+        {
+            continue;
+        }
+
+        while (arr[i] <= M)
+        {
+            M -= arr[i];
+            iCount++;
+        }
+    }
+
+
+    getch();
     return 0;
 }
