dc9966a49fc85474541f96b45952264d84ae7308
DESKTOP-E55IUE1\JJUNI <qkrghdwns98@naver.com>
2023-04-01 14:51:30 +0900

    백준 그리디 전자레인지 시간초

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -5,60 +5,49 @@
 #pragma warning(disable :4996)
 using namespace std;
 
-int N, M;
+int T, M;
 int main()
 {
     freopen("input.txt", "r", stdin);
 
-    vector<int> arr;
-    vector<int> arr2;
-    cin >> N;
+    cin >> T;
 
-    for (int i = 0; i < N; i++)
+    int A = 300;
+    int B = 60;
+    int C = 10;
+    
+    int iCountA = 0;
+    int iCountB = 0;
+    int iCountC = 0;
+    while (T != 0)
     {
-        int a;
-        cin >> a; 
-        arr.push_back(a);
-    }
-    for (int i = 0; i < N; i++)
-    {
-        int a;
-        cin >> a;
-        arr2.push_back(a);
-    }
-
-    sort(arr.rbegin(), arr.rend());
-
-    vector<int> Temp = arr2;
-    sort(Temp.begin(), Temp.end());
-    //A의 가장큰수는 B의 가장 작은수와 곱해져야함.
-
-    int iResult = 0;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        for (int j = 0; j < arr2.size(); j++)
+        if (T / A > 0)
         {
-            int k = Temp[i];
-
-            while(j < arr2.size())
-            {
-                if (k == arr2[j])
-                {
-                    iResult += arr2[j] * arr[i];
-                    arr2.erase(arr2.begin() + j);
-                    break;
-                }
-                else
-                {
-                    j++;
-                    continue;
-                }
-            }
-            break;
+            T -= A;
+            iCountA++;
+            continue;
         }
+
+        if (T / B > 0)
+        {
+            T -= B;
+            iCountB++;
+            continue;
+        }
+
+        if (T / C > 0)
+        {
+            T -= C;
+            iCountC++;
+            continue;
+        }
+
+        cout << -1 << endl;
+        return 0;
+        break;
     }
 
-    cout << iResult << endl;
+    cout << iCountA << " " << iCountB << " " << iCountC << endl;
     getch();
     return 0;
 }
