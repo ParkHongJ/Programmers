e8af75bc8238e5b7ab69e2ded822e10869d5f82f
DESKTOP-E55IUE1\JJUNI <qkrghdwns98@naver.com>
2023-04-02 21:27:41 +0900

    프로그래머스 광물캐기 하는중

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <vector>
-#include <conio.h>
 #include <algorithm>
+#include <conio.h>
 #pragma warning(disable :4996)
 using namespace std;
 
 int T, M;
-int main()
+void BackJoon()
 {
     freopen("input.txt", "r", stdin);
 
@@ -15,7 +18,7 @@ int main()
     int A = 300;
     int B = 60;
     int C = 10;
-    
+
     int iCountA = 0;
     int iCountB = 0;
     int iCountC = 0;
@@ -43,11 +46,58 @@ int main()
         }
 
         cout << -1 << endl;
-        return 0;
+        return;
         break;
     }
 
     cout << iCountA << " " << iCountB << " " << iCountC << endl;
+    //getch();
+}
+
+typedef struct tagMineralsFatigue {
+    int dia = 0;
+    int iron = 0;
+    int stone = 0;
+}Fatigue;
+
+int solution(vector<int> picks, vector<string> minerals) {
+    
+    vector< Fatigue> fatigue;
+
+    int iCount = 0;
+	Fatigue _fatigue;
+	for (int i = 0; i < minerals.size() - minerals.size() % 5; i++)
+	{
+		if (minerals[i] == "diamond") { _fatigue.dia++; }
+		if (minerals[i] == "iron") { _fatigue.iron++; }
+		if (minerals[i] == "stone") { _fatigue.stone++; }
+
+		iCount++;
+		if (iCount >= 5)
+		{
+            fatigue.push_back(_fatigue);
+            _fatigue.dia = 0;
+            _fatigue.iron = 0;
+            _fatigue.stone = 0;
+		}
+    }
+	sort(fatigue.begin(), fatigue.end(), [](int dia, int iron) -> bool {
+		return dia > iron;
+		});
+    int answer = 0;
+    return answer;
+}
+
+void Programmers()
+{
+    vector<int> picks = { 1, 3, 2 };
+    vector<string> minerals = { "diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone" };
+    cout << solution(picks, minerals) << endl;
+}
+
+int main()
+{
+    Programmers();
     getch();
     return 0;
 }
