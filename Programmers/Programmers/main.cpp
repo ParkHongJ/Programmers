2ee42216d96dc38e6af48712e8a0348e54ac52bd
ParkHongJ <qkrghdwns98@naver.com>
2025-03-27 09:16:40 +0900

    백준 숫자카드 1,2

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -16,122 +16,56 @@ using namespace std;
 void BackJoon()
 {
 	freopen("input.txt", "r", stdin);
-
-
+	
 	cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
 
-	int M, N;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
-	//나온 단어의 수
-
-	unordered_map<string, int> wordNum;
-
-	//map<int, string> wordLength;
-
+	map<int, int> HasCards;
 	for (int i = 0; i < N; i++)
 	{
-		string str;
-		cin >> str;
-		//M 이상인 단어들만
-		if (str.length() >= M)
+		int Input;
+		cin >> Input;
+		map<int, int>::iterator iter = HasCards.find(Input);
+		if (iter != HasCards.end())
 		{
-			unordered_map<string, int>::iterator iter = wordNum.find(str);
-
-			//이미 단어목록에 들어갔다면
-			if (iter != wordNum.end())
-			{
-				iter->second++;
-			}
-			else
-			{
-				wordNum.emplace(str, 1);
-			}
-		}
-	}
-
-	priority_queue<pair<int, string>, vector<pair<int, string>>, less<pair<int, string>>> pq; //부른횟수
-	for (auto& iter : wordNum)
-	{
-		pq.push(make_pair(iter.second, iter.first));
-	}
-
-	while (!pq.empty())
-	{
-		//가장 많이불린 단어.
-		pair<int, string> temp = pq.top();
-
-		pq.pop();
-
-		//다음 단어와 겹치는지 확인해야함.
-
-		//겹치는 단어 목록
-		set<string> collaps;		
-		//priority_queue<pair<int, string>, vector<pair<int, string>>, less<pair<int, string>>> lengthpq;
-		while (!pq.empty())
-		{
-			pair<int, string> temp2 = pq.top();
-
-			//겹친다
-			if (temp2.first == temp.first)
-			{
-				collaps.insert(temp2.second);
-				pq.pop();
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		//겹치는 단어가 없다면.
-		if (collaps.empty())
-		{
-			cout << temp.second << "\n";
+			++(iter->second);
 		}
 		else
 		{
-			//todo
-			//길이순
-			
-			//사전순
+			HasCards.emplace(Input, 1);
 		}
+		
 	}
+	
+	int M;
+	cin >> M;
 
-	/*int prevNum = 0;
-	while (!pq.empty())
+	vector<int> TargetCards;
+	TargetCards.reserve(M);
+
+	vector<int> Result;
+	Result.resize(M, 0);
+
+	for (int i = 0; i < M; i++)
 	{
-		pair<int, string> temp = pq.top();
+		int Input;
+		cin >> Input;
 
-		pq.pop();
+		map<int, int>::iterator iter = HasCards.find(Input);
 
-		vector<pair<int, string>> tempVec;
-		while (!pq.empty())
+		//소유하고있다면
+		if (iter != HasCards.end())
 		{
-			pair<int, string> next = pq.top();
-
-			if (temp.first == next.first)
-			{
-				tempVec.push_back(next);
-				pq.pop();
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		if (tempVec.empty())
-		{
-			cout << temp.second << "\n";
+			cout << 1 << " ";
 		}
 		else
 		{
-			
+			cout << 0 <<" ";
 		}
-
-	}*/
+	}
 }
 
 int main()
