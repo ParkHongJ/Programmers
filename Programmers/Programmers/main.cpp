29e80d075fadb4051343a56b16c29e3c5e36d006
ParkHongJ <qkrghdwns98@naver.com>
2025-04-30 12:30:45 +0900

    123

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -17,76 +17,48 @@ using namespace std;
 void BackJoon()
 {
 	freopen("input.txt", "r", stdin);
-	
+
 	cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
 	
 	int input;
 	cin >> input;
 
-	//다음 노드까지의 거리
-	vector<unsigned long long> toNextlength;
-
-	for (int i = 0; i < input - 1; ++i)
+	int charge = 1000 - input;
+	int result = 0;
+	if (charge / 500 > 0)
 	{
-		int length;
-		cin >> length;
-		toNextlength.push_back(length);
+		result += charge / 500;
+		charge -= charge / 500 * 500;
 	}
 
-	//각 주유소의 1리터당 기름
-	vector<unsigned long long> gasPrice;
-
-	for (int i = 0; i < input; i++)
+	if (charge / 100 > 0)
 	{
-		int gas;
-		cin >> gas;
-		gasPrice.push_back(gas);
+		result += charge / 100;
+		charge -= charge / 100 * 100;
 	}
 
-	//현재 연료 L
-	unsigned long long currentFuel = 0;
-	unsigned long long currentNode = 0;
-
-	unsigned long long totalCost = 0;
-	//연료 값이 현재보다 작은걸 만날때까지 앞으로 가야한다.
-	while (currentNode + 1 < gasPrice.size())
+	if (charge / 50 > 0)
 	{
-		//다음 노드 연료가 더 작으면
-		if (gasPrice[currentNode] > gasPrice[currentNode + 1])
-		{
-			totalCost += gasPrice[currentNode] * toNextlength[currentNode];
-		}
-		else
-		{
-			//다음 노드 연료가 더 크면
-
-			int length = 0;
-			int toNext = 0;
-			for (int i = currentNode; i < gasPrice.size(); ++i)
-			{
-				//작은 노드를 만날때까지 길이 계산
-				if (gasPrice[currentNode] > gasPrice[i])
-				{
-					//다음 노드가 더 작으면 끝
-					break;
-				}
-				else
-				{
-					if (i < toNextlength.size())
-					{
-						length += toNextlength[i];
-					}
-					++toNext;
-				}
-			}
-			totalCost += length* gasPrice[currentNode];
-			currentNode += toNext;
-			continue;
-		}
-		++currentNode;
+		result += charge / 50;
+		charge -= charge / 50 * 50;
 	}
-	cout << totalCost;
+
+	if (charge / 10 > 0)
+	{
+		result += charge / 10;
+		charge -= charge / 10 * 10;
+	}
+
+	if (charge / 5 > 0)
+	{
+		result += charge / 5;
+		charge -= charge / 5 * 5;
+	}
+
+	result += charge;
+
+	cout << result << endl;
 }
 
 
